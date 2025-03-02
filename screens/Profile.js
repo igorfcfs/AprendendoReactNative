6c7bd79233098b87118ac1e5ac45de84aa08b0f2
igorfcfs <igorfcfs@gmail.com>
2025-03-02 02:33:02 -0300

    Layouts com Flexbox, Componentes Funcionais X Baseados em Classe

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,15 +1,23 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import general from '../styles/general';
+import DimensoesFixasWidthHeightEFlexDirection from '../components/DimensoesFixasWidthHeightEFlexDirection';
+import DimensoesFixasJustifyContent from '../components/DimensoesFixasJustifyContent';
+import DimensoesFixasAlignItems from '../components/DimensoesFixasAlignItems';
+import DimensoesFixasFlexWrap from '../components/DimensoesFixasFlexWrap';
+import DimensoesFixasFlexGrow from '../components/DimensoesFixasFlexGrow';
+import DimensoesFixasFlexShrinkEBasis from '../components/DimensoesFixasFlexShrinkEBasis';
 
 export default function Profile() {
   return (
     <View style={styles.container}>
       <Text style={styles.sectionTitle}>Hello World!!!</Text>
+      <DimensoesFixasWidthHeightEFlexDirection />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  ...general
-})
+  ...general,
+});
